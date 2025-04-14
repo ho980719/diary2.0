c9ffb9b47b3bd076e5facc739cd3f6994ae90b3c
김준호 <wnsgh_s2@naver.com>
2025-04-14 17:05:58 +0900

    Revert "feat: List, View Dto 분리 (jackson Views)"
    This reverts commit 26a68c79f6992124d83a150049bfda0f140272ad.

--- diary-api/src/main/java/com/ho/diary/api/service/feed/FeedApiService.java
+++ diary-api/src/main/java/com/ho/diary/api/service/feed/FeedApiService.java
@@ -27,12 +27,7 @@ public class FeedApiService {
   @Transactional(readOnly = true)
   public List<FeedDto> getFeeds() {
     return feedService.getFeeds()
-      .stream().map(x -> {
-        List<CommonFileDto> images = commonFileService.getFiles(x.getId(), REFERENCE_TYPE)
-          .stream().map(CommonFileDto::from)
-          .toList();
-        return FeedDto.of(x, images);
-      })
+      .stream().map(FeedDto::of)
       .toList();
   }
 
