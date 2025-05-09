2cdd9b8379fc620baf8e53a871f450caa08a97ba
김준호 <wnsgh_s2@naver.com>
2025-05-09 14:16:26 +0900

    front: prettier 적용

--- diary-front/src/App.tsx
+++ diary-front/src/App.tsx
@@ -1,22 +1,22 @@
-import {Route, Routes} from "react-router-dom";
-import Index from "./pages";
-import Header from "./pages/layout/Header.tsx";
-import {Container} from "@mui/material";
-import Box from "@mui/material/Box";
+import {Route, Routes} from 'react-router-dom';
+import Index from './pages';
+import Header from './pages/layout/Header.tsx';
+import {Container} from '@mui/material';
+import Box from '@mui/material/Box';
 
 function App() {
     return (
         <>
-            <Box sx={{ maxWidth: 1, bgcolor: 'background.paper' }}>
+            <Box sx={{maxWidth: 1, bgcolor: 'background.paper'}}>
                 <Header/>
-                <Container maxWidth="lg">
+                <Container sx={{mt: 4}} maxWidth="lg">
                     <Routes>
                         <Route path="/" element={<Index/>}/>
                     </Routes>
                 </Container>
             </Box>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
