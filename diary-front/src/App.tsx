8e94fcea162845f2552a0ed1801e1c1f0514a7aa
김준호 <wnsgh_s2@naver.com>
2025-05-09 13:53:14 +0900

    front: use mui & layout toolbar set

--- diary-front/src/App.tsx
+++ diary-front/src/App.tsx
@@ -1,15 +1,22 @@
-import './App.css'
 import {Route, Routes} from "react-router-dom";
-import SignUp from "./pages/users/SignUp.tsx";
 import Index from "./pages";
+import Header from "./pages/layout/Header.tsx";
+import {Container} from "@mui/material";
+import Box from "@mui/material/Box";
 
 function App() {
-  return (
-      <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/users/sign-up" element={<SignUp />} />
-      </Routes>
-  )
+    return (
+        <>
+            <Box sx={{ maxWidth: 1, bgcolor: 'background.paper' }}>
+                <Header/>
+                <Container maxWidth="lg">
+                    <Routes>
+                        <Route path="/" element={<Index/>}/>
+                    </Routes>
+                </Container>
+            </Box>
+        </>
+    )
 }
 
 export default App
