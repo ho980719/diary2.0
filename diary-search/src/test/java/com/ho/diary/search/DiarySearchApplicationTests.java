6710e397266e58c7eac1dfe1d9c2b6a088f64d5d
김준호 <wnsgh_s2@naver.com>
2025-05-14 12:34:11 +0900

    feat: diary-search setting

---
+++ diary-search/src/test/java/com/ho/diary/search/DiarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ho.diary.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarySearchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
